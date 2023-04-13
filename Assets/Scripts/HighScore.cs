1bbd2c63dac028f11b5158d08441e3f6fe030e68
fredericogarcia <fredericomelogarcia@outlook.com>
2023-04-13 14:26:15 +0100

    tidy up

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -26,7 +26,7 @@ public class HighScore : MonoBehaviour
         if (!timer.paused && !player.victory) elapsedTime += Time.deltaTime; // starts timer
         else
         {   // if the timer is paused and the player is in a victory screen
-            // we then check if there is a highscore or none, if so, we set the new highscore time, we also use the levelIndex so we can track highscores for
+            // we then check if there is a highscore or none, if so, we set the new highscore time, we also use the levelIndex so we can track highscore for
             // multiple levels
             if (elapsedTime < highScore || highScore == 0) PlayerPrefs.SetFloat("highscore" + levelIndex, elapsedTime); 
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Components")]
     private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
     private Animator animator;
     private Timer timer;
     [Header("Movement")] 
     [SerializeField] private float movementSpeed;
-    [SerializeField] private bool IsMoving;
+    [SerializeField] private bool isMoving;
     private bool canMove;
     private Vector2 pInput;
     [Header("UI")]
-    [SerializeField] private GameObject VictoryScreen;
-    [SerializeField] private GameObject GameOverScreen;
+    [SerializeField] private GameObject victoryScreen;
+    [SerializeField] private GameObject gameOverScreen;
 
     public bool victory;
+    private static readonly int X = Animator.StringToHash("X");
+    private static readonly int Y = Animator.StringToHash("Y");
+    private static readonly int Victory = Animator.StringToHash("Victory");
+    private static readonly int Death = Animator.StringToHash("Death");
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         timer = FindObjectOfType<Timer>();
         canMove = true;
@@ -31,17 +33,17 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // manages players input setting the correct values on the blendtree so the animations will play correctly
+        // manages players input setting the correct values on the blend tree so the animations will play correctly
         rb.velocity = pInput * movementSpeed;
-        switch (IsMoving)
+        switch (isMoving)
         {
             case true:
-                animator.SetFloat("X", pInput.x);
-                animator.SetFloat("Y", pInput.y);
+                animator.SetFloat(X, pInput.x);
+                animator.SetFloat(Y, pInput.y);
                 break;
             case false:
-                animator.SetFloat("X", 0f);
-                animator.SetFloat("Y", 0f);
+                animator.SetFloat(X, 0f);
+                animator.SetFloat(Y, 0f);
                 break;
         }
         if (victory) // if the player has won show victory screen and stops any possible input
@@ -57,7 +59,7 @@ public class PlayerController : MonoBehaviour
         if (canMove)
         {
             pInput = context.ReadValue<Vector2>();
-            IsMoving = pInput != Vector2.zero;
+            isMoving = pInput != Vector2.zero;
         }
     }
     // checks for collision on the shrine
@@ -68,21 +70,21 @@ public class PlayerController : MonoBehaviour
     // respawns player if no time left
     public IEnumerator Respawn()
     {
-        animator.SetTrigger("Death");
+        animator.SetTrigger(Death);
         timer.paused = true;
         StopPlayerInput();
         yield return new WaitForSeconds(2f);
-        GameOverScreen.SetActive(true);
+        gameOverScreen.SetActive(true);
         
     }
     // plays wining dance and shows victory screen
     private IEnumerator Win()
     {
-        animator.SetTrigger("Victory");
+        animator.SetTrigger(Victory);
         timer.paused = true;
         StopPlayerInput();
         yield return new WaitForSeconds(2f);
-        VictoryScreen.SetActive(true);
+        victoryScreen.SetActive(true);
     }
     // used in the traps script so I could update the players speed
     public void UpdateMovementSpeed(float value)
