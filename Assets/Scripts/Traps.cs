85b41adcca96e66945564b84367b48ca2f77eb28
fredericogarcia <fredericomelogarcia@outlook.com>
2023-04-25 09:40:50 +0100

    traps animations

--- Assets/Scripts/Traps.cs
+++ Assets/Scripts/Traps.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -13,25 +14,26 @@ public class Traps : MonoBehaviour
     private Gradient localOldGradient;
     [SerializeField] private TrailRenderer trail;
 
-    // when player goes over trap start IEnumerator passing down the value of the collider
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player")) StartCoroutine(SlowThenSpeedUpPlayerOvertime(other));
-    }
+    private PlayerController player;
 
-    private IEnumerator SlowThenSpeedUpPlayerOvertime(Collider2D player)
+    private void Awake() => player = FindObjectOfType<PlayerController>();
+
+    // when player goes over trap start IEnumerator passing down the value of the collider
+    private void OnTriggerEnter2D(Collider2D other) => StartCoroutine(SlowThenSpeedUpPlayerOvertime());
+
+    private IEnumerator SlowThenSpeedUpPlayerOvertime()
     {
         // save players current movement speed
-        var savedSpeed = player.GetComponent<PlayerController>().GetMovementSpeed();
+        var savedSpeed = player.GetMovementSpeed();
         // take 1 from players movement speed to slow player down
-        player.GetComponent<PlayerController>().UpdateMovementSpeed(-1);
+        player.UpdateMovementSpeed(-1);
         // change the color of the trail renderer
         trail.colorGradient = newGradient;
         yield return new WaitForSeconds(2f);
         // slowly increase players speed after a 2s delay
-        while (player.GetComponent<PlayerController>().GetMovementSpeed() < savedSpeed)
+        while (player.GetMovementSpeed() < savedSpeed)
         {
-            player.GetComponent<PlayerController>().UpdateMovementSpeed(0.25f);
+            player.UpdateMovementSpeed(0.25f);
             yield return new WaitForSeconds(1f);
         }
         // change trail renderer color back to original color
